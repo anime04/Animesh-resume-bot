c92167af4c7d014ed31dade7ee718fbec6ab021d
Anime04 <jainan1450@gmail.com>
2026-08-27 23:15:11 +0530

    Allow all Vercel domains in CORS regex configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ for dev_port in [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
+    allow_origin_regex=r"(http://(localhost|127\.0\.0\.1)(:\d+)?|https://.*\.vercel\.app)",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
